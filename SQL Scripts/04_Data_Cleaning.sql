b89b5914121acb2d6241307f361d398ab142f3dd
Saransh Wakode <saranshwakode@gmail.com>
2026-08-07 16:30:32 +0530

    Update 04_Data_Cleaning.sql

--- SQL Scripts/04_Data_Cleaning.sql
+++ SQL Scripts/04_Data_Cleaning.sql
@@ -1,1 +1,28 @@
+USE Netflix_db
+--==============================
+-- ANALYSING THE DATA----
+--==============================
+
+SELECT TOP 10 *
+FROM NETFLIX
+
+--======================
+--COUNTING TOTAL RECORDS ROWS
+--======================
+
+SELECT 
+COUNT (*) AS TOTAL_ROWS 
+FROM NETFLIX
+
+--============================
+--CHECKING FOR DUPLICATE ROWS 
+--============================
+SELECT 
+SHOW_ID ,
+COUNT (*) AS DUPLICATE_ROWS 
+FROM NETFLIX
+GROUP BY SHOW_ID 
+HAVING COUNT (*) > 1 
+--=======================
+
 
