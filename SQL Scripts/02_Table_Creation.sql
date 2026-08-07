526887ecd00a2cc218b1473af31bfc10a80588e1
Saransh Wakode <saranshwakode@gmail.com>
2026-08-07 15:51:11 +0530

    Update 02_Table_Creation.sql

--- SQL Scripts/02_Table_Creation.sql
+++ SQL Scripts/02_Table_Creation.sql
@@ -1,1 +1,18 @@
+--------NETFLIX PROJECT -------
+
+DROP TABLE IF EXISTS Netflix;
+CREATE TABLE Netflix (
+    show_id VARCHAR(10),
+    type VARCHAR(20),
+    title VARCHAR(300),
+    director VARCHAR(500),
+    casts VARCHAR(MAX),
+    country VARCHAR(300),
+    date_added VARCHAR(50),
+    release_year INT,
+    rating VARCHAR(20),
+    duration VARCHAR(50),
+    listed_in VARCHAR(300),
+    description VARCHAR(MAX)
+);
 
